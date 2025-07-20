14fe3942b646b3b8ac17704f93edb5fecb4a39de
Kawai Cheng <cheng.kw0123@gmail.com>
2025-07-20 17:22:45 +0800

    Fixed forgetpassword.tsx for vercel deployment

--- src/app/forget-pw/ForgotPassword.tsx
+++ src/app/forget-pw/ForgotPassword.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Router, { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import { supabase } from '../supabaseClient';
 import './ForgotPassword.css';
 import { Input } from "@/components/ui/input"
